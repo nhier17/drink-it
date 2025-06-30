e183af0f759dd4977a7d5a23f7ba22faf12bbb43
Abraham <abrahamnhier97@gmail.com>
2025-06-30 18:13:20 +0300

    implemented art page

--- components/features/Art.tsx
+++ components/features/Art.tsx
@@ -31,47 +31,46 @@ const Art = () => {
 
     return (
         <div id="art">
-            <div className="container mx-auto h-full pt-2">
-                <h2 className="will-fade">The Art</h2>
-
-                <div className="content">
-                    <ul className="space-y-4 will-fade">
-                        {goodLists.map((feature, index) => (
-                            <li key={index} className="flex items-center gap-2">
-                                <Image src="/images/check.png" alt="check" width={100} height={100} />
-                                <p>{feature}</p>
-                            </li>
-                        ))}
-                    </ul>
-
-                    <div className="cocktail-img">
-                        <Image 
-                        src="/images/under-img.jpg" 
-                        alt="cocktail" 
-                        fill
-                        className="abs-center masked-img size-full object-contain"
-                        />
-                    </div>
-
-                    <ul className="space-y-4 will-fade">
-                        {featureLists.map((feature, index) => (
-                            <li key={index} className="flex items-center justify-start gap-2">
-                                <Image src="/images/check.png" alt="check" fill />
-                                <p>{feature}</p>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-
-                <div className="masked-container">
-                    <h2 className="will-fade">Sip-Worthy Perfection</h2>
-                    <div id="masked-content">
-                        <h3>Made with Craft, Poured with Passion</h3>
-                        <p>This isn’t just a drink. It’s a carefully crafted moment made just for you.</p>
-                    </div>
-                </div>
+        <div className="container mx-auto h-full pt-20">
+           <h2 className="will-fade">The ART</h2>
+           
+           <div className="content">
+            <ul className="space-y-4 will-fade">
+               {goodLists.map((feature, index) => (
+                <li key={index} className="flex items-center gap-2">
+                   <Image src="/images/check.png" alt="check" width={20} height={20} />
+                   <p>{feature}</p>
+                </li>
+               ))}
+            </ul>
+            
+            <div className="cocktail-img">
+               <img
+                   src="/images/under-img.jpg"
+                   alt="cocktail"
+                   className="abs-center masked-img size-full object-contain"
+               />
             </div>
+            
+            <ul className="space-y-4 will-fade">
+               {featureLists.map((feature, index) => (
+                <li key={index} className="flex items-center justify-start gap-2">
+                   <Image src="/images/check.png" alt="check" width={20} height={20} />
+                   <p className="md:w-fit w-60">{feature}</p>
+                </li>
+               ))}
+            </ul>
+           </div>
+           
+           <div className="masked-container">
+            <h2 className="will-fade">Sip-Worthy Perfection</h2>
+            <div id="masked-content">
+               <h3>Made with Craft, Poured with Passion</h3>
+               <p>This isn’t just a drink. It’s a carefully crafted moment made just for you.</p>
+            </div>
+           </div>
         </div>
+       </div>
     );
 };
 
