c6922f322f81722a82870cf617879da9b5a25591
Abraham <abrahamnhier97@gmail.com>
2025-06-30 09:29:15 +0300

    feat: utility classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function Home() {
-
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex-center">
       <h1>Drink It</h1>
     </div>
   );
